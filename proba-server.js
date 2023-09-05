8e7b897728c5eda37fdaf284b8bc6d4b6822a446
star\Z <everest4us@gmail.com>
2023-09-05 23:03:34 +0300

    add proba server

---
+++ proba-server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+ 
+const port = 3001;
+ 
+const server = http.createServer(async (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+  
+    res.end('<h1>Hello World</h1>');
+});
+ 
+server.listen(port, () => {
+    console.log(`Server running...`);
+});
